cb7c47ab1ebba4bc122b1c90862985954bfb03b1
Dmitry Popov <dmitrytga@yandex.ru>
2025-10-04 01:26:22 +0300

    first build

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -16,5 +16,5 @@
     <SvgoArrow class="m-8 rotate-180" />
   </div>
   <div class="m-8 font-bold text-4xl">Login page</div>
-  <img class="m-8 w-200" src="~assets/images/who.gif"></img>
+  <img class="m-8 w-200" src="~assets/images/who.jpg"></img>
 </template>
