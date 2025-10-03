3506e75829903f953b96759e62cb6e5d589a3791
Dmitry Popov <dmitrytga@yandex.ru>
2025-10-04 01:40:45 +0300

    feature: added SpeedInsights

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,16 @@
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+
+definePageMeta({
+  layout: 'default',
+})
+</script>
+
+
 <template>
   <div class="DefaultLayout">
     <h1 class="m-8 text-5xl">Default layout</h1>
+    <SpeedInsights />
     <slot />
   </div>
 </template>
