ad689a6e0df96ca5dd4b530ecab1cadeca5c29bc
Dmitry Popov <dmitrytga@yandex.ru>
2025-10-04 01:54:38 +0300

    fix: fixed issue with Vercel Speed Insights

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,3 @@
-<script setup lang="ts">
-import { SpeedInsights } from "@vercel/speed-insights/nuxt"
-
-definePageMeta({
-  layout: 'default',
-})
-</script>
-
-
 <template>
   <div class="DefaultLayout">
     <h1 class="m-8 text-5xl">Default layout</h1>
