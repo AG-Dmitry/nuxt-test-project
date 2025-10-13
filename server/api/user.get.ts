1003b0735dcff4c8b8fb7f8f9897cd811aa3ef05
Dmitry Popov <dmitrytga@yandex.ru>
2025-10-13 10:38:31 +0300

    test: update user.get endpoint to include email field

--- server/api/user.get.ts
+++ server/api/user.get.ts
@@ -1,7 +1,7 @@
 import { stackServerApp } from '../auth/stack';
 
 export default defineEventHandler(async () => {
-  //const name = 'f3cf12cd-723c-4d1f-a459-cf0e095cf115';
-  //const response = await stackServerApp.getUser(name);
-  return { user: 'ping', signal: 'pong' };
+  const name = 'f3cf12cd-723c-4d1f-a459-cf0e095cf115';
+  const response = await stackServerApp.getUser(name);
+  return { user: response, signal: 'pong' };
 });
