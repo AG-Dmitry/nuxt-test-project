ac96f684ce628002c617b65c01f44d36a99e9512
Dmitry Popov <dmitrytga@yandex.ru>
2025-10-13 09:00:37 +0300

    fix: replaces $fetch with useFetch in useUtils.ts

--- app/composables/useUtils.ts
+++ app/composables/useUtils.ts
@@ -3,11 +3,11 @@ import useUserModel from '~/stores/userModel';
 export const useUtils = () => {
   const signinUser = async (email: string, password: string) => {
     const userModel = useUserModel();
-    const signInResult = await $fetch('/api/user/signin', {
+    const { data: signInResult} = await useFetch('/api/user/signin', {
       method: 'POST',
       query: { email: email, password: password },
     });
-    if (signInResult.userEmail) userModel.loginUser(signInResult.userEmail);
+    if (signInResult.value?.userEmail) userModel.loginUser(signInResult.value.userEmail);
     console.log(signInResult);
   };
   return { signinUser };
