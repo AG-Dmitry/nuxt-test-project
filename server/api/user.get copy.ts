e47cfc2827b0b54a0578ca9ea41debf50f79fc59
Dmitry Popov <dmitrytga@yandex.ru>
2025-10-13 10:36:04 +0300

    test: add copy of user.get.ts for testing

---
+++ server/api/user.get copy.ts
@@ -0,0 +1,9 @@
+import { stackServerApp } from '../auth/stack';
+
+export default defineEventHandler(async () => {
+  //const query = getQuery(e);
+  //const name = query.name as string;
+  const name = 'f3cf12cd-723c-4d1f-a459-cf0e095cf115';
+  const response = await stackServerApp.getUser(name);
+  return { user: response, signal: 'pong' };
+});
--- server/api/user.get.ts
+++ server/api/user.get.ts
@@ -1,9 +1,7 @@
 import { stackServerApp } from '../auth/stack';
 
 export default defineEventHandler(async () => {
-  //const query = getQuery(e);
-  //const name = query.name as string;
-  const name = 'f3cf12cd-723c-4d1f-a459-cf0e095cf115';
-  const response = await stackServerApp.getUser(name);
-  return { user: response, signal: 'pong' };
+  //const name = 'f3cf12cd-723c-4d1f-a459-cf0e095cf115';
+  //const response = await stackServerApp.getUser(name);
+  return { user: 'ping', signal: 'pong' };
 });
