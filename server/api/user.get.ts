ac367fc19d43c6aeb00424b4596afcb71c2fda7b
Dmitry Popov <dmitrytga@yandex.ru>
2025-10-13 10:16:03 +0300

    fix: added username in user.get API

--- server/api/user.get.ts
+++ server/api/user.get.ts
@@ -1,9 +1,10 @@
 import 'dotenv/config';
 import { stackServerApp } from '../auth/stack';
 
-export default defineEventHandler(async (e) => {
-  const query = getQuery(e);
-  const name = query.name as string;
+export default defineEventHandler(async () => {
+  //const query = getQuery(e);
+  //const name = query.name as string;
+  const name = 'f3cf12cd-723c-4d1f-a459-cf0e095cf115';
   const response = await stackServerApp.getUser(name);
   return { user: response };
 });
